5d2fff87e9e09fe0fd57f80b292a82b5f68a5b48
glitrma <237988427@qq.com>
2022-07-03 12:28:23 +0800

    - 选择城市组件

--- src/components/chooseCity/src/index.vue
+++ src/components/chooseCity/src/index.vue
@@ -15,39 +15,51 @@
       <el-row>
         <el-col :span="8">
           <el-radio-group v-model="radioValue" size="small">
-            <el-radio-button label="按城市" >按城市</el-radio-button>
-            <el-radio-button label="按省份" >按省份</el-radio-button>
+            <el-radio-button label="按城市">按城市</el-radio-button>
+            <!-- <el-radio-button label="按省份" >按省份</el-radio-button> -->
           </el-radio-group>
         </el-col>
         <el-col :offset="1" :span="15">
           <el-select
+            @change="changeSelect"
             size="small"
             v-model="selectValue"
             filterable
-            placeholder="Select"
+            placeholder="请搜索"
+            :filter-method="filterMethod"
           >
             <el-option
               v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
+              :key="item.id"
+              :label="item.name"
+              :value="item.id"
             />
           </el-select>
         </el-col>
       </el-row>
       <div class="city">
         <!-- 字母区域 -->
-        <div class="city-item" v-for="(value, index) in Object.keys(cities)" @click="clickChat(value)" :key="index">
+        <div
+          class="city-item"
+          v-for="(value, index) in Object.keys(cities)"
+          @click="clickChat(value)"
+          :key="index"
+        >
           {{ value }}
         </div>
       </div>
       <el-scrollbar max-height="300px">
         <template v-for="(value, key) in cities" :key="key">
-          <el-row style="margin-bottom: 10px;" :id="key">
-            <el-col :span="2">{{key}}：</el-col>
+          <el-row style="margin-bottom: 10px" :id="key">
+            <el-col :span="2">{{ key }}：</el-col>
             <el-col :span="22" class="city-name">
-              <div v-for="(item) in value" @click="clickItem(item)" class="city-name-item" :key="item.id">
-                {{item.name}}
+              <div
+                v-for="item in value"
+                @click="clickItem(item)"
+                class="city-name-item"
+                :key="item.id"
+              >
+                {{ item.name }}
               </div>
             </el-col>
           </el-row>
@@ -58,7 +70,7 @@
 </template>
   
 <script setup lang='ts'>
-import { ref } from "vue";
+import { ref, onMounted, watch } from "vue";
 import city from "../lib/city";
 import { City } from "./types";
 
@@ -69,32 +81,13 @@ let result = ref<string>("请选择");
 // 控制弹出层的显示
 let visible = ref<boolean>(false);
 // 单选框的值 按城市还是按省份选择
-let radioValue = ref("按城市");
+let radioValue = ref<string>("按城市");
 // 下拉框的值 搜索下拉框
 let selectValue = ref<string>("");
 // 下拉框显示城市的数据
-const options = ref([
-  {
-    value: "Option1",
-    label: "Option1",
-  },
-  {
-    value: "Option2",
-    label: "Option2",
-  },
-  {
-    value: "Option3",
-    label: "Option3",
-  },
-  {
-    value: "Option4",
-    label: "Option4",
-  },
-  {
-    value: "Option5",
-    label: "Option5",
-  },
-]);
+const options = ref<City[]>([]);
+// 搜索输入框的值
+let searchValue = ref<string>("")
 
 // 所有的城市数据
 let cities = ref(city.cities);
@@ -114,6 +107,33 @@ let clickChat = (item: string) => {
   if (el) el.scrollIntoView()
 }
 
+// 监听输入框的值
+watch(() => searchValue.value, val => {
+  filterMethod
+})
+
+onMounted(() => {
+  let values = Object.values(cities.value).flat(2)
+  options.value = values
+})
+
+let filterMethod = (val: string) => {
+  let values = Object.values(cities.value).flat(2)
+  searchValue.value = val
+  options.value = values.filter(item => {
+    return item.name.includes(val)
+  })
+}
+
+let changeSelect = (val: City) => {
+  let allCities = Object.values(cities.value).flat(2)
+  let city = allCities.find(item => item.id === val)
+  console.log(city)
+  result.value = city?.name + ''
+  visible.value = false
+
+  emits('changeValue', city?.name)
+}
 // let radioChange = (data: any) => {
 //   radioValue.value = data
 //   console.log(data, '====数据====');
@@ -154,11 +174,11 @@ svg {
     cursor: pointer;
   }
 }
-.city-name{
+.city-name {
   display: flex;
   align-items: center;
   flex-wrap: wrap;
-  &-item{
+  &-item {
     margin-right: 6px;
     margin-bottom: 6px;
     cursor: pointer;
